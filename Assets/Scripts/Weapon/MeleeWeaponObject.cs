eb5d31accb3e982a6a6ecf1c5f5cc46001ead3dd
Brett LoCicero <62044317+brettlocicero@users.noreply.github.com>
2026-03-15 18:21:24 -0400

    fix attack origin

--- Assets/Scripts/Weapon/MeleeWeaponObject.cs
+++ Assets/Scripts/Weapon/MeleeWeaponObject.cs
@@ -4,10 +4,8 @@ public class MeleeWeaponObject : WeaponObject
 {
     protected override void Attack(WeaponAttack attack)
     {
-        Vector3 center = transform.position + transform.forward * 1.5f;
-
         int enemyLayer = LayerMask.GetMask("Enemy");
-        Collider[] hits = Physics.OverlapSphere(center, weaponSO.range, enemyLayer);
+        Collider[] hits = Physics.OverlapSphere(attackOrigin.position, weaponSO.range, enemyLayer);
 
         foreach (Collider hit in hits)
         {
