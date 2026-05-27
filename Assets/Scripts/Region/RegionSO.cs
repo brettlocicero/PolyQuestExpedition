f8f70e0030b4455f2d17a35584ae3f6d902ec29a
Brett LoCicero <62044317+brettlocicero@users.noreply.github.com>
2026-05-26 21:00:48 -0400

    add Buto; new region objects

--- Assets/Scripts/Region/RegionSO.cs
+++ Assets/Scripts/Region/RegionSO.cs
@@ -7,24 +7,26 @@ public class RegionSO : ScriptableObject
     public EnemyAI[] enemies;
 
     [Header("Environment")]
-    public GameObject groundObject;
+    public GameObject regionObject;
 
     [Header("VFX")]
     public Material skybox;
     public Color fogColor;
     public Color sunColor;
+    public Color intensityColor;
 
     public void ApplyRegionVFX()
     {
         RenderSettings.skybox = skybox;
         RenderSettings.fogColor = fogColor;
         RenderSettings.sun.color = sunColor;
+        RenderSettings.ambientSkyColor = intensityColor;
     }
 
     public GameObject SpawnRegion()
     {
         GameObject regionObj = new GameObject(regionName + " Object");
-        GameObject groundObj = Instantiate(groundObject, Vector3.zero, Quaternion.identity);
+        GameObject groundObj = Instantiate(regionObject, Vector3.zero, Quaternion.identity);
         groundObj.transform.SetParent(regionObj.transform);
 
         return regionObj;
