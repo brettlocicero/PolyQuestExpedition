42bdfaa47ae6498d9edb74fbd58da5a06644c9a0
Brett LoCicero <62044317+brettlocicero@users.noreply.github.com>
2026-05-29 18:10:11 -0400

    revert infinite world

--- Assets/Scripts/Region/RegionManager.cs
+++ Assets/Scripts/Region/RegionManager.cs
@@ -1,5 +1,4 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class RegionManager : MonoBehaviour
@@ -14,54 +13,14 @@ public class RegionManager : MonoBehaviour
     [SerializeField] EnemySpawner enemySpawner;
     [SerializeField] GameObject hubObjects;
     [SerializeField] Animator regionEntranceAnim;
-    [SerializeField] Transform player;
-
-    [Header("Infinite Generation")]
-    [SerializeField] int worldSeed = 12345;
-    [SerializeField] float chunkSize = 200f;
-    [SerializeField] int loadRadius = 2;
 
     GameObject regionObj;
     RegionSO currentRegion;
-    Vector2Int currentPlayerChunk;
-    readonly Dictionary<Vector2Int, GameObject> loadedChunks = new Dictionary<Vector2Int, GameObject>();
-    Coroutine enterRegionCoroutine;
-
-    void OnValidate()
-    {
-        chunkSize = Mathf.Max(1f, chunkSize);
-        loadRadius = Mathf.Max(0, loadRadius);
-    }
-
-    void Update()
-    {
-        if (!currentRegion || !regionObj)
-            return;
-
-        if (!player && PlayerInstance.instance)
-            player = PlayerInstance.instance.transform;
-
-        if (!player)
-            return;
-
-        Vector2Int playerChunk = WorldToChunk(player.position);
-        if (playerChunk == currentPlayerChunk && loadedChunks.Count > 0)
-            return;
-
-        currentPlayerChunk = playerChunk;
-        UpdateLoadedChunks();
-    }
 
     public void EnterRegion(RegionSO region)
     {
-        if (enterRegionCoroutine != null)
-            StopCoroutine(enterRegionCoroutine);
-
-        if (regionObj)
-            Destroy(regionObj);
-
-        loadedChunks.Clear();
-        enterRegionCoroutine = StartCoroutine(EnterRegionWorker(region));
+        if (regionObj) Destroy(regionObj);
+        StartCoroutine(EnterRegionWorker(region));
     }
 
     IEnumerator EnterRegionWorker(RegionSO region)
@@ -72,85 +31,9 @@ public class RegionManager : MonoBehaviour
 
         currentRegion = region;
         region.ApplyRegionVFX();
-        regionObj = new GameObject(region.regionName + " Object");
+        regionObj = region.SpawnRegion();
         hubObjects.SetActive(false);
 
-        if (!player && PlayerInstance.instance)
-            player = PlayerInstance.instance.transform;
-
-        currentPlayerChunk = player ? WorldToChunk(player.position) : Vector2Int.zero;
-        UpdateLoadedChunks();
-
         enemySpawner.StartSpawnLoop();
-        enterRegionCoroutine = null;
-    }
-
-    Vector2Int WorldToChunk(Vector3 position)
-    {
-        return new Vector2Int(Mathf.FloorToInt(position.x / chunkSize), Mathf.FloorToInt(position.z / chunkSize));
-    }
-
-    void UpdateLoadedChunks()
-    {
-        HashSet<Vector2Int> chunksToKeep = new HashSet<Vector2Int>();
-        for (int x = -loadRadius; x <= loadRadius; x++)
-        {
-            for (int z = -loadRadius; z <= loadRadius; z++)
-            {
-                Vector2Int chunkCoord = currentPlayerChunk + new Vector2Int(x, z);
-                chunksToKeep.Add(chunkCoord);
-
-                if (!loadedChunks.ContainsKey(chunkCoord))
-                    LoadChunk(chunkCoord);
-            }
-        }
-
-        List<Vector2Int> chunksToUnload = new List<Vector2Int>();
-        foreach (Vector2Int chunkCoord in loadedChunks.Keys)
-        {
-            if (!chunksToKeep.Contains(chunkCoord))
-                chunksToUnload.Add(chunkCoord);
-        }
-
-        foreach (Vector2Int chunkCoord in chunksToUnload)
-        {
-            Destroy(loadedChunks[chunkCoord]);
-            loadedChunks.Remove(chunkCoord);
-        }
-    }
-
-    void LoadChunk(Vector2Int chunkCoord)
-    {
-        int chunkSeed = GetChunkSeed(chunkCoord);
-        GameObject chunkObj = currentRegion.SpawnChunk(chunkCoord, chunkSize, chunkSeed, regionObj.transform);
-        loadedChunks.Add(chunkCoord, chunkObj);
-    }
-
-    int GetChunkSeed(Vector2Int chunkCoord)
-    {
-        unchecked
-        {
-            int hash = worldSeed;
-            hash = hash * 397 ^ GetStableStringHash(currentRegion.regionName);
-            hash = hash * 397 ^ chunkCoord.x;
-            hash = hash * 397 ^ chunkCoord.y;
-            return hash;
-        }
-    }
-
-    int GetStableStringHash(string value)
-    {
-        unchecked
-        {
-            int hash = 23;
-
-            if (string.IsNullOrEmpty(value))
-                return hash;
-
-            for (int i = 0; i < value.Length; i++)
-                hash = hash * 31 + value[i];
-
-            return hash;
-        }
     }
 }
--- Assets/Scripts/Region/RegionSO.cs
+++ Assets/Scripts/Region/RegionSO.cs
@@ -40,21 +40,6 @@ public class RegionSO : ScriptableObject
         return regionObj;
     }
 
-    public GameObject SpawnChunk(Vector2Int chunkCoord, float chunkSize, int seed, Transform parent)
-    {
-        Vector3 chunkOrigin = new Vector3(chunkCoord.x * chunkSize, 0f, chunkCoord.y * chunkSize);
-        GameObject chunkObj = new GameObject(regionName + " Chunk " + chunkCoord.x + ", " + chunkCoord.y);
-        chunkObj.transform.SetParent(parent);
-        chunkObj.transform.position = chunkOrigin;
-
-        GameObject groundObj = Instantiate(regionObject, chunkOrigin, Quaternion.identity);
-        groundObj.transform.SetParent(chunkObj.transform);
-
-        SpawnPropsInChunk(chunkObj, chunkOrigin, chunkSize, seed);
-
-        return chunkObj;
-    }
-
     public void SpawnProps(GameObject regionObj)
     {
         Vector3 center = regionObj.transform.position;
@@ -83,42 +68,5 @@ public class RegionSO : ScriptableObject
                 }
             }
         }
-    }
-
-    void SpawnPropsInChunk(GameObject chunkObj, Vector3 chunkOrigin, float chunkSize, int seed)
-    {
-        System.Random random = new System.Random(seed);
-
-        foreach (PropSO prop in props)
-        {
-            for (int i = 0; i < prop.maxAmount; i++)
-            {
-                float x = chunkOrigin.x + NextFloat(random, 0f, chunkSize);
-                float z = chunkOrigin.z + NextFloat(random, 0f, chunkSize);
-                Vector3 rayOrigin = new Vector3(x, chunkOrigin.y + 500f, z);
-
-                if (Physics.Raycast(rayOrigin, Vector3.down, out RaycastHit hit, 1000f, groundLayer))
-                {
-                    Vector3 pos = new Vector3(hit.point.x, hit.point.y + prop.yOffset, hit.point.z);
-                    GameObject spawnedProp = Instantiate(prop.propObj, pos, Quaternion.identity, chunkObj.transform);
-                    if (prop.randomizeScale)
-                    {
-                        float randomScale = NextFloat(random, prop.scaleRange.x, prop.scaleRange.y);
-                        spawnedProp.transform.localScale *= randomScale;
-                    }
-
-                    if (prop.randomizeRotation)
-                    {
-                        float randomYRotation = NextFloat(random, prop.rotationRange.x, prop.rotationRange.y);
-                        spawnedProp.transform.rotation = Quaternion.Euler(0f, randomYRotation, 0f);
-                    }
-                }
-            }
-        }
-    }
-
-    float NextFloat(System.Random random, float min, float max)
-    {
-        return Mathf.Lerp(min, max, (float)random.NextDouble());
     }
 }
