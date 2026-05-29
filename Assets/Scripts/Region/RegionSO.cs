5a1bffa6b1c74c48e9db01d74d198cf4b63e6b07
Brett LoCicero <62044317+brettlocicero@users.noreply.github.com>
2026-05-28 21:29:05 -0400

    add infinite world

--- Assets/Scripts/Region/RegionSO.cs
+++ Assets/Scripts/Region/RegionSO.cs
@@ -5,6 +5,7 @@ public class RegionSO : ScriptableObject
 {
     public string regionName;
     public float regionRadius = 1000f;
+    public LayerMask groundLayer;
 
     [Header("")]
     public EnemyAI[] enemies;
@@ -39,6 +40,21 @@ public class RegionSO : ScriptableObject
         return regionObj;
     }
 
+    public GameObject SpawnChunk(Vector2Int chunkCoord, float chunkSize, int seed, Transform parent)
+    {
+        Vector3 chunkOrigin = new Vector3(chunkCoord.x * chunkSize, 0f, chunkCoord.y * chunkSize);
+        GameObject chunkObj = new GameObject(regionName + " Chunk " + chunkCoord.x + ", " + chunkCoord.y);
+        chunkObj.transform.SetParent(parent);
+        chunkObj.transform.position = chunkOrigin;
+
+        GameObject groundObj = Instantiate(regionObject, chunkOrigin, Quaternion.identity);
+        groundObj.transform.SetParent(chunkObj.transform);
+
+        SpawnPropsInChunk(chunkObj, chunkOrigin, chunkSize, seed);
+
+        return chunkObj;
+    }
+
     public void SpawnProps(GameObject regionObj)
     {
         Vector3 center = regionObj.transform.position;
@@ -49,7 +65,7 @@ public class RegionSO : ScriptableObject
                 Vector2 randomCircle = Random.insideUnitCircle * regionRadius;
                 Vector3 rayOrigin = new Vector3(center.x + randomCircle.x, center.y + 500f, center.z + randomCircle.y);
 
-                if (Physics.Raycast(rayOrigin, Vector3.down, out RaycastHit hit, 1000f))
+                if (Physics.Raycast(rayOrigin, Vector3.down, out RaycastHit hit, 1000f, groundLayer))
                 {
                     Vector3 pos = new Vector3(hit.point.x, hit.point.y + prop.yOffset, hit.point.z);
                     GameObject spawnedProp = Instantiate(prop.propObj, pos, Quaternion.identity, regionObj.transform);
@@ -67,5 +83,42 @@ public class RegionSO : ScriptableObject
                 }
             }
         }
+    }
+
+    void SpawnPropsInChunk(GameObject chunkObj, Vector3 chunkOrigin, float chunkSize, int seed)
+    {
+        System.Random random = new System.Random(seed);
+
+        foreach (PropSO prop in props)
+        {
+            for (int i = 0; i < prop.maxAmount; i++)
+            {
+                float x = chunkOrigin.x + NextFloat(random, 0f, chunkSize);
+                float z = chunkOrigin.z + NextFloat(random, 0f, chunkSize);
+                Vector3 rayOrigin = new Vector3(x, chunkOrigin.y + 500f, z);
+
+                if (Physics.Raycast(rayOrigin, Vector3.down, out RaycastHit hit, 1000f, groundLayer))
+                {
+                    Vector3 pos = new Vector3(hit.point.x, hit.point.y + prop.yOffset, hit.point.z);
+                    GameObject spawnedProp = Instantiate(prop.propObj, pos, Quaternion.identity, chunkObj.transform);
+                    if (prop.randomizeScale)
+                    {
+                        float randomScale = NextFloat(random, prop.scaleRange.x, prop.scaleRange.y);
+                        spawnedProp.transform.localScale *= randomScale;
+                    }
+
+                    if (prop.randomizeRotation)
+                    {
+                        float randomYRotation = NextFloat(random, prop.rotationRange.x, prop.rotationRange.y);
+                        spawnedProp.transform.rotation = Quaternion.Euler(0f, randomYRotation, 0f);
+                    }
+                }
+            }
+        }
+    }
+
+    float NextFloat(System.Random random, float min, float max)
+    {
+        return Mathf.Lerp(min, max, (float)random.NextDouble());
     }
 }
